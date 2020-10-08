5b9a14b9b4bb83752e5f63729e62849f94106494
WhyBecause <maxence.traina@gmail.com>
2020-10-08 23:59:24 +0200

    fix suite

--- index.js
+++ index.js
@@ -48,41 +48,7 @@ mongoose.connection.on('open', () => {
 })
 
    
-// Pour créer les Roles dans la DB lorsqu'ils n'existent pas
-    // Role.estimatedDocumentCount((err, count) => {
-    //   if (!err && count === 0) {
-    //     new Role({
-    //       name: "user",
-    //     }).save((err) => {
-    //       if (err) {
-    //         console.log("error", err);
-    //       }
-  
-    //       console.log("added 'user' to roles collection");
-    //     });
-  
-    //     new Role({
-    //       name: "moderator",
-    //     }).save((err) => {
-    //       if (err) {
-    //         console.log("error", err);
-    //       }
-  
-    //       console.log("added 'moderator' to roles collection");
-    //     });
-  
-    //     new Role({
-    //       name: "admin",
-    //     }).save((err) => {
-    //       if (err) {
-    //         console.log("error", err);
-    //       }
-  
-    //       console.log("added 'admin' to roles collection");
-    //     });
-    //   }
-    // });
-  
+
 
 
 
